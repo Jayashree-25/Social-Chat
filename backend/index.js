6b152ddade040ab5ecdd000b8e03885ab5497e28
jayashre-25 <dasjayashree8260@gmail.com>
2025-07-10 17:22:22 +0530

    env

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send("API is running...");
+});
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewParser : true,
+    useUnifiedTopology : true,
+}).then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`Running at port ${PORT}`));
+}).catch((err) => console.error("error in MongoDB connection"));
+
+//mongodb+srv://jayashree:socialchat123@cluster0.cxlv8.mongodb.net/social_chat?retryWrites=true&w=majority&appName=Cluster0
