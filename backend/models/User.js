5be60fee73cda96766053c512e816044cef9f67f
jayashre-25 <dasjayashree8260@gmail.com>
2025-07-11 15:32:06 +0530

    mongoose model

---
+++ backend/models/User.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: function(){
+            return !this.googleId; //password required only if not signed in with Google
+        }
+    },
+    //is for google login support(OAuth 2.0), when user logs in.. Google gives a unique id and we store it here
+    googleId: {
+        type: String,
+        default: null
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
