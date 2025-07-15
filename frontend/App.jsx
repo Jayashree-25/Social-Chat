e753b91b7f36d192b0313a7996b474f5bb40ff9a
jayashre-25 <dasjayashree8260@gmail.com>
2025-07-15 15:26:49 +0530

    error fixing

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { GoogleLogin } from "@react-oauth/google";
+import React from 'react';
+import { GoogleLogin } from '@react-oauth/google';
 
 const App = () => {
   return (
--- frontend/main.jsx
+++ frontend/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const clientId = "http://232617022214-ul4up1faq5l5ev7dbbj05mrddd76uk2b.apps.googleusercontent.com";
+const clientId = "232617022214-ul4up1faq5l5ev7dbbj05mrddd76uk2b.apps.googleusercontent.com";
 
-ReactDOM.createRoot(document.getElementById(" root ")).render(
-    <GoogleOAuthProvider clientId= {clientId} >
-        <App />
-    </GoogleOAuthProvider>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider clientId={clientId}>
+    <App />
+  </GoogleOAuthProvider>
+);
