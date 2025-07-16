df5c71ec69f4803bc74ed5c07a0ab9f5f0e67e59
jayashre-25 <dasjayashree8260@gmail.com>
2025-07-16 19:52:42 +0530

    installed google auth lib and google OAuth route added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import User from '../models/User.js';
 import verifyToken from '../middleware/verifyToken.js';
+import { OAuth2Client } from 'google-auth-library';
 
 const router = express.Router();
 
@@ -77,6 +78,47 @@ router.post('/login', async (req, res) => {
 // GET - Current logged-in user
 router.get('/me', verifyToken, (req, res) => {
     res.status(200).json({ user: req.user });
-})
+});
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+// POST - Google OAuth Route
+router.post('/google', async (req, res) => {
+    try {
+        const { credential } = req.body;
+
+        const ticket = await client.verifyIdToken({
+            idToken: credential,
+            audience: process.env.GOOGLE_CLIENT_ID,
+        });
+
+        const payload = ticket.getPayload();
+        const { sub, email, name } = payload;
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            user = new User({
+                name,
+                email,
+                googleId: sub,
+            });
+            await user.save();
+        }
+
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: '7d' }
+        );
+
+        res.status(200).json({
+            token,
+            user: { name: user.name, email: user.email },
+        });
+    } catch (err) {
+        console.error('Google OAuth Error:', err.message);
+        res.status(500).json({ message: 'Google login failed' });
+    }
+});
 
 export default router;
