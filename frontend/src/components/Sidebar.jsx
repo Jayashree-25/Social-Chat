6655109066d946fa3ef2f6d43138f7614992c82a
jayashre-25 <dasjayashree8260@gmail.com>
2025-07-17 23:30:07 +0530

    updated left sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,42 +1,69 @@
-import React from "react";
-import { List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import React, { useContext } from "react";
+import { List, ListItem, ListItemIcon, ListItemText, Box, Typography } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import LogoutIcon from "@mui/icons-material/Logout";
+import { UserContext } from "./UserWrapper";
 
 const Sidebar = () => {
+    const { currentUser } = useContext(UserContext);
+
     const handleLogout = () => {
         localStorage.removeItem("token");
         window.location.href = "/";
     };
 
     return (
-        <div style={{
-            width: "250px",
-            height: "100vh",
-            background: "#f2f2f2",
-            paddingTop: "20px"
-        }}>
+        <Box
+            sx={{
+                width: "20%",
+                backgroundColor: "#fdfdfd",
+                padding: "20px",
+                boxShadow: 3,
+                borderRadius: "16px",
+                margin: "20px",
+                display: "flex",
+                flexDirection: "column",
+                height: "calc(100vh - 40px)",
+            }}
+        >
+            {/* Stylish App Title */}
+            <Typography
+                variant="h5"
+                fontWeight="bold"
+                sx={{
+                    textAlign: "center",
+                    fontFamily: "'Pacifico', cursive",
+                    color: "#1976d2",
+                    mb: 4,
+                }}
+            >
+                Social Chat
+            </Typography>
+
             <List>
                 <ListItem button>
                     <ListItemIcon><HomeIcon /></ListItemIcon>
                     <ListItemText primary="Home" />
                 </ListItem>
+
                 <ListItem button>
                     <ListItemIcon><NotificationsIcon /></ListItemIcon>
-                    <ListItemText primary="Notification" />
+                    <ListItemText primary="Notifications" />
                 </ListItem>
+
                 <ListItem button>
                     <ListItemIcon><AccountCircleIcon /></ListItemIcon>
                     <ListItemText primary="Profile" />
                 </ListItem>
+
                 <ListItem button onClick={handleLogout}>
                     <ListItemIcon><LogoutIcon sx={{ color: "red" }} /></ListItemIcon>
                     <ListItemText primary="Logout" sx={{ color: "red" }} />
                 </ListItem>
             </List>
-        </div>
-    )
+        </Box>
+    );
 }
 export default Sidebar;
