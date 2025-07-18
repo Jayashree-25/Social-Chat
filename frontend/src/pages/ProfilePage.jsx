78b5fe5e912362f11e976d3912fc5549fa0dfc5f
jayashre-25 <dasjayashree8260@gmail.com>
2025-07-18 18:06:42 +0530

    ProfilePage dummy

---
+++ frontend/src/pages/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProfilePage = () => {
+    return (
+        <div style={{ padding: "2rem" }}>
+            <h2>Profile Page</h2>;
+            <p>Here details will go on..</p>
+        </div>
+    )
+}
+
+export default ProfilePage;
