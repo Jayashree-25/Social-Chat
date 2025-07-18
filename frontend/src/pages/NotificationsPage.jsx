f21cb28eb9f8a4ded2ddc665432ec60ae099ec4e
jayashre-25 <dasjayashree8260@gmail.com>
2025-07-18 18:33:22 +0530

    NotificationsPage dummy

---
+++ frontend/src/pages/NotificationsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotificationsPage = () => {
+    return (
+        <div style={{ padding: "2rem"}}>
+            <h2>Notification Page</h2>;
+            <p>Notification will be shown..</p>
+        </div>
+    )
+}
+
+export default NotificationsPage;
